8027257dce5e392c5c3b677240d28466c0a82010
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-08-24 13:05:35 +0100

    JWT-springSecurity

---
+++ backend/src/main/java/com/backend/auth/AuthenticationService.java
@@ -0,0 +1,49 @@
+package com.backend.auth;
+
+import com.backend.config.JwtService;
+import com.backend.model.Role;
+import com.backend.model.User;
+import com.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.TEACHER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ backend/src/main/java/com/backend/model/Role.java
@@ -0,0 +1,6 @@
+package com.backend.model;
+
+public enum Role {
+    TEACHER,
+    STUDENT
+}
