c7b94579b51db4710f53677b040fd50de762154b
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-09-08 22:07:15 +0100

    Config

--- backend/src/main/java/com/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/backend/config/SecurityConfiguration.java
@@ -10,11 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -60,16 +55,5 @@ public class SecurityConfiguration{
         return http.build();
 
     }
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:4200"); // Allow requests from this origin
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
-    }
 }
