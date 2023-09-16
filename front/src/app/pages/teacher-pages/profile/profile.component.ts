38c979b26659c7a71ac1477f85cc9824aadd8599
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-09-16 01:23:54 +0100

    ProfileEdit

--- front/src/app/pages/teacher-pages/profile/profile.component.ts
+++ front/src/app/pages/teacher-pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user';
 import { UserServiceService } from 'src/app/services/user-service.service';
 
@@ -10,6 +11,12 @@ interface professor{
   image:string;
   numberStudents:number;
   numberCourses:number;
+}
+export interface UserUpdate{
+  firstName:''
+  lastName:''
+  email:''
+  password:''
 }
 @Component({
   selector: 'app-profile',
@@ -34,14 +41,54 @@ user:User = {
   role: '',
   enabled: false
 };
-constructor(private userService:UserServiceService){
+userUpdated: UserUpdate= {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+};
+passwordForm: FormGroup;
+
+constructor(private userService:UserServiceService,private formBuilder: FormBuilder){
   const authUserJSON:any = localStorage.getItem("authUser");
     this.user = JSON.parse(authUserJSON);
+    this.passwordForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+    });
 }
-update(){
-  //update user here
+
+onUpdateUser(){
+  
+this.userService.updateUser(this.userUpdated,this.user.id).subscribe(
+  {
+     next:(response) =>{
+      console.log("success")
+      if(this.userUpdated.firstName !== ""){
+        this.user.firstName = this.userUpdated.firstName;
+      }
+     }, 
+     error: (error) => console.log(error)
+  }
+)
 }
+updatePassword() {
+  const passwordControl = this.passwordForm.get('password');
+  if (passwordControl !== null && this.passwordForm.valid) {
+    const newPassword = passwordControl.value;
+    //je fais ca parce que c'est toujours userUpdated que je dois passer en parametre pour la fonction updateUser
+    this.userUpdated.password = newPassword;
+    //ici j'appelle la fonction 
+    this.userService.updateUser(this.userUpdated,this.user.id).subscribe({
+      next:(response) => console.log("success"),
+      error:(e) => console.log(e)
+    })
+  } else {
+    this.passwordForm.markAllAsTouched();
+  }
+}
+
 ngOnInit(){
-  console.log("salut",this.user.id)
+  console.log("salut",this.user.lastName)
 }
 }
