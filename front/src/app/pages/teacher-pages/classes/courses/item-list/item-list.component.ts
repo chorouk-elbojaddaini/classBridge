706ad9661520918d6ae1fd8580d909a06816e1ab
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-09-20 20:20:15 +0100

    ItemList

--- front/src/app/pages/teacher-pages/classes/courses/item-list/item-list.component.ts
+++ front/src/app/pages/teacher-pages/classes/courses/item-list/item-list.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Course } from 'src/app/models/course';
+import { CourseService } from 'src/app/services/course.service';
 import { SelectedItemService } from 'src/app/services/selected-item-service.service';
+import { SharedDataService } from 'src/app/services/shared-data.service';
 
 @Component({
   selector: 'app-item-list',
@@ -7,6 +10,8 @@ import { SelectedItemService } from 'src/app/services/selected-item-service.serv
   styleUrls: ['./item-list.component.scss']
 })
 export class ItemListComponent {
+  @Input() classe: any;
+  courses?:any;
   items = [
     {
       title: 'Chapitre 1 Introduction au réseau',
@@ -53,14 +58,38 @@ export class ItemListComponent {
   ];
   showOptions: boolean = false;
   selected:boolean = false;
-  constructor(private selectedItemService: SelectedItemService) {}
+  selectedClass:any;
+  selectedItem: any = null;
+  constructor(private selectedItemService: SelectedItemService,private sharedDataService:SharedDataService,private courseService:CourseService) {
+    this.selectedClass = this.sharedDataService.getSelectedClass(); // Récupérez la valeur depuis le service
+     console.log("claaaassse f item list",this.selectedClass);
+  }
 
-  toggleOptions() {
+  toggleOptions(item:any) {
     this.showOptions = !this.showOptions;
+    this.selectedItem = item;
   }
 
   onItemClick(item: any) {
     this.selected = !this.selected;
     this.selectedItemService.setSelectedItem(item);
   }
+
+  ngOnInit(){
+    this.courseService.getAllCourses(this.selectedClass.idClass).subscribe({
+      next:(Response)=>{
+        this.courses = Response;
+        console.log(this.courses[0])
+      },
+      error:(error)=> console.log(error)
+    });
+
+
+   
+  }
+
+  formatDate(dateStr: string): string {
+    return dateStr.split("T")[0];
+  }
+
 }
