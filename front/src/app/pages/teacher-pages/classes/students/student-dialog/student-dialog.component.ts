c4f38434afb004ce1a116fd61ff8019202dc0cc3
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-09-21 22:58:22 +0100

    Students-front

--- front/src/app/pages/teacher-pages/classes/students/student-dialog/student-dialog.component.ts
+++ front/src/app/pages/teacher-pages/classes/students/student-dialog/student-dialog.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Student } from 'src/app/models/student';
 import { SharedDataService } from 'src/app/services/shared-data.service';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-student-dialog',
@@ -10,9 +13,26 @@ export class StudentDialogComponent {
 
   selectedClass:any;
  class:any;
-  constructor(private sharedDataservice:SharedDataService){
-     this.class = localStorage.getItem('idClass');
-     console.log("classss ddd",this.class);
+ studentSelected:any;
+ isEditing = false;
+ editedNote: number | any;
+
+ startEditing(student:Student) {
+   this.isEditing = true;
+   this.editedNote = student.note; 
+ }
+
+ updateNote(student:Student) {
+   student.note = this.editedNote;
+   this.isEditing = false;
+  //  this.editedNote = null;
+   this.studentService.updateNote(student.id,this.editedNote).subscribe({
+    next:(response) => console.log(response),
+    error:(error) => console.log(error)
+   })
+ }
+  constructor(private sharedDataservice:SharedDataService,@Inject(MAT_DIALOG_DATA) public data: { student: Student },private studentService:StudentService){
+     this.studentSelected = data.student;
   }
 
 }
--- front/src/app/pages/teacher-pages/classes/students/students.component.ts
+++ front/src/app/pages/teacher-pages/classes/students/students.component.ts
@@ -1,30 +1,91 @@
 import { Component } from '@angular/core';
 import { Student } from 'src/app/models/student';
 import studentsData from '../../../../../assets/data/studentsData.json';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { StudentDialogComponent } from './student-dialog/student-dialog.component';
 import { SharedDataService } from 'src/app/services/shared-data.service';
+import { StudentService } from 'src/app/services/student.service';
+import { S, er } from '@fullcalendar/core/internal-common';
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.scss']
 })
 export class StudentsComponent {
-  students : Student[] = studentsData;
+  students : Student[] =[];
   selectedClass:any;
   classCode:any;
-   constructor(private dialog: MatDialog,private sharedDataService:SharedDataService) {
+  numberStudents:number = 0;
+  deleteBtn:boolean = false;
+
+   constructor(private dialog: MatDialog,private sharedDataService:SharedDataService,private studentService:StudentService) {
     this.selectedClass = this.sharedDataService.getSelectedClass(); 
     this.classCode = localStorage.getItem('classCode');
     console.log("classe code",this.classCode);
    }
+   ngDoCheck(){
+    if(this.deleteBtn){
+      this.getAllStudents();
+      this.deleteBtn = false;
+    }
+   }
   ngOnInit(){
     console.log(this.students);
+    this.getAllStudents();
+
   }
-  openDialog(){
-    const dialogRef = this.dialog.open(StudentDialogComponent);
- 
+
+
+  openDialog(student: Student){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = { student }; 
+  
+    const dialogRef = this.dialog.open(StudentDialogComponent, dialogConfig);
+  
     dialogRef.afterClosed().subscribe((_result: any) => {
     });
+  }
+
+
+  getAllStudents(){
+    this.studentService.findByCodeClass(this.classCode).subscribe({
+      next:(Response:any) => {
+        this.numberStudents = Response.length;
+        this.studentService.findClass(this.classCode).subscribe({
+          next:(classe:any) => {
+            this.students.forEach(student => {
+              student.fieldName = classe.fieldName;
+              student.level = classe.level;
+            });
+        }})
+        this.students = Response.map((objet:any) => {
+          return {
+            id:objet.id,
+            firstName: objet.user.firstName,
+            lastName: objet.user.lastName,
+            email: objet.user.email,
+            note: objet.note, 
+          };
+        });
+      console.log("response hadi",this.students)},
+      error:(error) => console.log(error)
+    })
+  }
+
+
+
+  deleteStudent(id:number){
+ 
+      this.studentService.deleteStudent(id).subscribe({
+      next:() =>{
+        alert("Étudiant supprimé avec succès!");
+        console.log("deleted successfully!");
+        this.deleteBtn = true;
+      },
+      error:(e) =>{
+        alert("ID de l'utilisateur n'existe pas");
+        console.log("User Id doesn't exist");
+      }
+    })
   }
 }
