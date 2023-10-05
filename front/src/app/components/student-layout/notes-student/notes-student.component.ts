a5bcd2a3332c796c2d74e0713a5e69a64622cb6c
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-10-05 23:12:02 +0100

    Update

--- front/src/app/components/student-layout/notes-student/notes-student.component.ts
+++ front/src/app/components/student-layout/notes-student/notes-student.component.ts
@@ -14,7 +14,7 @@ export interface Notes{
 })
 export class NotesStudentComponent {
   tableauDeDonnees = [
-    { nomClasse: 'Classe A', nomProfesseur: 'Professeur 1', note: 18 },
+    { nomClasse: 'Classe A', nomProfesseur: 'Professeur 1', note: 10 },
     { nomClasse: 'Classe B', nomProfesseur: 'Professeur 2', note: 15 },
     { nomClasse: 'Classe C', nomProfesseur: 'Professeur 3', note: 20 },
   ];
